6133f76991d021633aa8443958c6750dbca336cd
DarkAngel <marcelo@darkblue.tv>
2019-08-14 21:39:57 -0300

    add ActionsEnm.h

---
+++ src/HouzCore/ActionsEnm.h
@@ -0,0 +1,13 @@
+//////////////////////////////////////
+// IMPORTANT: changes must be reflected on client side
+
+#define action_ack          0x00
+#define action_rfSentOk     0x01
+#define action_rfSentRetry  0x02
+#define action_rfSentFail   0x03
+#define action_rfReceived   0x04
+#define action_conn         0x05
+#define action_irReceived   0x10
+#define action_wsConnected  0x11
+#define action_wsOffline    0x12
+#define action_rfOnline     0x14
