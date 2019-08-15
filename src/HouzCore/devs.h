04b1159dcb004ee02d80d42598054d2fc05095e4
DarkAngel <marcelo@darkblue.tv>
2019-08-14 21:32:52 -0300

    remove deviceCodecs

--- src/HouzCore/devs.h
+++ src/HouzCore/devs.h
@@ -50,19 +50,6 @@
 #define frontdesk_node		0x5
 
 
-///////////////////////////////////////////////////////////////////
-// rf setup
-#define rfChannel		0x5B   
-#define rf_server_tx	0xA0
-#define rf_server_rx	0xB0
-#define rf_office_tx	0xA1
-#define rf_office_rx	0xB1
-#define rf_suite_tx		0xA2
-#define rf_suite_rx		0xB2
-#define rf_living_tx	0xA3
-#define rf_living_rx	0xB3
-#define rf_wemos_tx		0xA4
-#define rf_wemos_rx		0xB4
 
 #define deviceType_generic  0
 #define deviceType_float    1
