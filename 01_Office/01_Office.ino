b2579ec6b3dabaaa1fe1bffa9358093f6c079e02
DarkAngel <marcelo@darkblue.tv>
2019-08-19 14:23:16 -0300

    add office node

---
+++ 01_Office/01_Office.ino
@@ -0,0 +1,21 @@
+#include "01_Office.h"
+#include "Arduino.h"
+
+/////////////////////////////
+// Wiring
+//
+// A0 LightSensor
+// D0 Wall Switch
+// D1 SCL (bme280)
+// D2 SDA (bme280)
+// D3 Wall StatusLed
+// D4 builtIn led
+// D5 relay out
+
+void setup(){
+
+};
+void loop(){
+
+};
+
