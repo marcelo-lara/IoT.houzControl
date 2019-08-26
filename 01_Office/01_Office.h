d3a3f0cf4cd67537741d1ea25933b18c2b319ed4
DarkAngel <marcelo@darkblue.tv>
2019-08-26 19:42:11 -0300

    office board hardware changes

--- 01_Office/01_Office.h
+++ 01_Office/01_Office.h
@@ -1,13 +1,13 @@
 #pragma once
 // Wiring
 #define lightSensor A0 // A0 LightSensor
-#define wallSwitch  14 // D0 Wall Switch
+#define wallSwitch  13 // D5 Wall Switch
 #define bme280_scl   5 // D1 SCL (bme280)
 #define bme280_sda   4 // D2 SDA (bme280)
 #define statusLed    0 // D3 Wall StatusLed
 #define wifiLed      2 // D4 builtIn led
 #define relayOut    12 // D6 relay out
-#define irSendPin   13 // D7 infrared signal out
+#define irSendPin   14 // D7 infrared signal out
 
 #include "src/HouzCore/HouzButton.h"
 #include "src/HouzCore/HouzCore.h"
